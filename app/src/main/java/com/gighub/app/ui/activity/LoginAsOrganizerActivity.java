db812e8bf365b372c015bb89f7c59de0cf8c1876
Paklek <ramadiansyahd@gmail.com>
2016-08-30 21:03:12 +0700

    new theme

---
+++ app/src/main/java/com/gighub/app/ui/activity/LoginAsOrganizerActivity.java
@@ -0,0 +1,13 @@
+package com.gighub.app.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginAsOrganizerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_as_organizer);
+    }
+}
---
+++ app/src/main/java/com/gighub/app/ui/activity/RegisterAsOrganizerActivity.java
@@ -0,0 +1,13 @@
+package com.gighub.app.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterAsOrganizerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_as_organizer);
+    }
+}
